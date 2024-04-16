163f439bf630cb70761861ccbbb9aa4495091fd7
Vithuran <132158797+vithuran12@users.noreply.github.com>
2024-04-16 11:17:01 +0530

    first commmit
    filament

---
+++ storage/framework/views/2e2120eeeca5760ddc8dd99e0fa4f535.php
@@ -0,0 +1,8 @@
+<div
+    <?php echo e($attributes->class(['fi-no-notification-body overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400'])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</div>
+<?php /**PATH T:\Notes\php-laravel\filament-yelo-code\vendor\filament\notifications\src\/../resources/views/components/body.blade.php ENDPATH**/ ?>
